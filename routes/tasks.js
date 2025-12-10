5330c22dab371b6f4fa29e193ec779cd5a2746fd
OussamaDjebbour <oussama_djebbour@outlook.com>
2025-12-10 17:06:44 +0100

    Set up the api with CRUD operations

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,13 @@
-import express from 'express'
-export const router = express.Router()
-import {getAllTasks,createTask,getTask,updateTask,deleteTask}  from '../controllers/tasks.js'
+import express from 'express';
+export const router = express.Router();
+import {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} from '../controllers/tasks.js';
 
+router.route('/').get(getAllTasks).post(createTask);
 
-
- router.route('/').get(getAllTasks).post(createTask)
-
- router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
-
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
